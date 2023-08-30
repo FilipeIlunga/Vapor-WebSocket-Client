e206f603751025ff899296286cde953415b4a175
filipeilunga <filipeilunga@gmail.com>
2023-08-30 13:38:34 -0300

    Initial Commit

---
+++ Vapor-Sockets/Vapor_SocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vapor_SocketsApp.swift
+//  Vapor-Sockets
+//
+//  Created by Filipe Ilunga on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Vapor_SocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
